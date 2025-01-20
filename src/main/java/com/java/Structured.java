33d10b3f49ea8616d66d623fd7cba6023a96195b
Duc Phan <phanquiduc@gmail.com>
2025-01-20 19:46:48 +0700

    Initial

---
+++ src/main/java/com/java/Structured.java
@@ -0,0 +1,24 @@
+package com.java;
+
+import java.lang.reflect.Array;
+import java.util.List;
+
+/**
+ * Structured
+ *
+ * @author Admin
+ * @version 1.0
+ * @since 2025-01-20
+ */
+public class Structured {
+	public static void main(String[] args) {
+		printAllNumbersInListStructured(List.of(12,45,3,5,2));
+	}
+	
+	private static void printAllNumbersInListStructured(List<Integer> numbers) {
+		// How to loop the numbers
+		for (int number : numbers){
+			System.out.println(number);
+		}
+	}
+}
