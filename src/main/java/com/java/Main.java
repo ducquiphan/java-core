15d1137c017e8b4950530ec9091601ddc6cd2b58
Duc Phan <phanquiduc@gmail.com>
2025-01-20 19:42:16 +0700

    Initial

---
+++ src/main/java/com/java/Main.java
@@ -0,0 +1,14 @@
+package com.java;
+
+/**
+ * ${NAME}
+ *
+ * @author Admin
+ * @version 1.0
+ * @since 2025-01-20
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
