235d40ac8f17486c0cd642f3c3e61232cbabb757
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2026-01-19 22:34:47 +0700

    lesson: Primitive Data Types

--- src/main/java/com/java/PrimitiveDataType.java
+++ src/main/java/com/java/PrimitiveDataType.java
@@ -9,12 +9,12 @@ package com.java;
  */
 public class PrimitiveDataType {
 	public static void main(String[] args) {
-		
-		byte a = 116;
-		short b = 12310;
-		int c = 123123123;
-		//		long d = 50000L + (10L * (a + b + c));
-		long d = (long) 50000 + (long) (10 * (a + b + c));
+		// Whole number
+		byte myByteValue = 116;
+		short myShortValue = 12310;
+		int myIntValue = 123123123;
+		//		long myLongValue = 50000L + (10L * (a + b + c));
+		long myLongValue = (long) 50000 + (long) (10 * (myByteValue + myShortValue + myIntValue));
 		double e = 10 / 2;
 		// 566 in binary (32-bit int): 00000000 00000000 00000010 00110110
 		// When you cast an int to a byte, Java simply discards everything except the last 8 bits (the least significant bits).
@@ -22,8 +22,16 @@ public class PrimitiveDataType {
 		// 566 / 256 = 2 remainder 54
 		byte f = (byte) (100 + 466);
 		System.out.println("Result of f: " + f);
-		System.out.println("Result of c: " + c);
-		System.out.println("Result of d: " + d);
+		System.out.println("Result of myIntValue: " + myIntValue);
+		System.out.println("Result of d: " + myLongValue);
 		System.out.println("Result of e: " + e);
+		
+		// Real number
+		//		float myFloatValue = 5;
+		float myFloatValue = 5f;
+		//		double myDoubleValue = 5;
+		double myDoubleValue = 5d;
+		System.out.println("Result of myFloatValue: " + myFloatValue);
+		System.out.println("Result of myDoubleValue: " + myDoubleValue);
 	}
 }
