5fd234af105d3bbccae768f37a6ed78e3f3f624b
Duc Phan <122254088+ducquiphan@users.noreply.github.com>
2026-01-25 00:14:52 +0700

    42. Java Operator Precedence: Mastering Expression Evaluation and Challenge

--- src/main/java/com/java/StatementAndConditionalLogic.java
+++ src/main/java/com/java/StatementAndConditionalLogic.java
@@ -10,9 +10,12 @@ package com.java;
 public class StatementAndConditionalLogic {
 	public static void main(String[] args) {
 		lessonTernaryOperator();
+		lessonPrecedenceOperator();
 	}
 	
 	static void lessonTernaryOperator() {
+		System.out.println("---------------------");
+		System.out.println("lessonTernaryOperator: ");
 		String makeOfCar = "Lamborghini";
 		//		boolean isDomesticCar = makeOfCar == "Lamborghini" ? false : true;
 		boolean isDomesticCar = (makeOfCar != "Lamborghini");
@@ -21,5 +24,23 @@ public class StatementAndConditionalLogic {
 		}
 		String result = isDomesticCar ? "This car is domestic" : "This car is not domestic";
 		System.out.println(result);
+		System.out.println();
+	}
+	
+	static void lessonPrecedenceOperator() {
+		System.out.println("---------------------");
+		System.out.println("lessonPrecedenceOperator: ");
+		double firstDoubleNum = 20.00d;
+		double secondDoubleNum = 80.00d;
+		double result = (firstDoubleNum + secondDoubleNum) * 100.00d;
+		System.out.println("Result is: " + result);
+		double remainder = result % 40.00d;
+		System.out.println("Remainder is: " + remainder);
+		boolean isDividedBy40 = remainder == 0;
+		System.out.println("isDividedBy40: " + isDividedBy40);
+		if (!isDividedBy40) {
+			System.out.println("Got some remainder");
+		}
+		System.out.println();
 	}
 }
